7b684e8b3f2d9161d7a73df33e7f49ab08301b7a
DanielaRocha6 <d.rocha@uniandes.edu.co>
2019-08-16 23:19:18 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 function updatePhotos(){
-    let top=7;
+    let max=8;
     let i=1;
     setInterval(function(){
-        if(i==top){
-          i=1;
-        }
+        if(i==top){ i=1;}
         document.getElementById("photos-family-friends").setAttribute("src","img/"+i+".jpg");
         i++;
       }, 1300);
