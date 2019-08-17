1e825cd55a2e010172055e31241a9b1b1ec12f2c
DanielaRocha6 <d.rocha@uniandes.edu.co>
2019-08-17 12:19:43 -0500

    correccion

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ function updatePhotos(){
     let max=8;
     let i=1;
     setInterval(function(){
-        if(i==top){ i=1;}
+        if(i==max){ i=1;}
         document.getElementById("photos-family-friends").setAttribute("src","./img/"+i+".jpg");
         i++;
-      }, 1300);
+      }, 1400);
   }
+
+  
