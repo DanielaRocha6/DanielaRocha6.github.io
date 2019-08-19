eb0e963c3a7842ccde7d885d5de5706c4cf2dbaf
DanielaRocha6 <d.rocha@uniandes.edu.co>
2019-08-18 21:58:20 -0500

    Cambios varios

--- js/canvas.js
+++ js/canvas.js
@@ -1,143 +1,113 @@
 
-var doc = document.getElementById("drawing");
-console.log(doc);
-var dibujo = doc.getContext("2d");
+var doc = document.getElementById('drawing')
+var dibujo = doc.getContext('2d')
 
-var doc2 = document.getElementById("drawing-2");
-console.log(doc2);
-var dibujo2 = doc2.getContext("2d");
+var doc2 = document.getElementById('drawing-2')
+var dibujo2 = doc2.getContext('2d')
 
-var doc3 = document.getElementById("drawing-3");
-console.log(doc3);
-var dibujo3 = doc3.getContext("2d");
+var doc3 = document.getElementById('drawing-3')
+var dibujo3 = doc3.getContext('2d')
 
-function draw() {
-    "use strict";
-    var numLines = 20;
-    var color1="#4dbd92";
-    var color2="#258a63 ";
+// eslint-disable-next-line no-unused-vars
+function draw () {
+  'use strict'
+  var numLines = 20
+  var color1 = '#4dbd92'
+  var color2 = '#258a63'
+  var color3 = '#FF0000'
+  var color4 = '#f06795'
 
-    var color3="#FF0000";
-    var color4="#f06795";
+  var color5 = '#50337a'
+  var color6 = '#ba95cc'
 
-    var color5="#50337a";
-    var color6="#ba95cc";
+  drawLine1(color1, 0, 0, 0, 200)
+  drawLine1(color1, 0, 0, 200, 0)
+  drawLine1(color1, 0, 200, 200, 200)
+  drawLine1(color1, 200, 200, 200, 0)
 
-    drawLine1(color1,0,0,0,200);
-    drawLine1(color1,0,0,200,0);
-    drawLine1(color1,0,200,200,200);
-    drawLine1(color1,200,200,200,0);
-
-    for(var i=0; i<numLines; i++) {
-        var y1 = i*10;
-        var x2 = 10*(i+1);
-        if(i%2==0)
-        {
-            drawLine1(color1,0,y1,x2,200);
-            drawLine2(color3,0,y1,x2,200);
-            drawLine3(color5,0,y1,x2,200);
-            
-        }
-        else
-        {
-            drawLine1(color2,0,y1,x2,200);
-            drawLine2(color4,0,y1,x2,200);
-            drawLine3(color6,0,y1,x2,200);
-            
-        }
-       
+  for (var i = 0; i < numLines; i++) {
+    var y1 = i * 10
+    var x2 = 10 * (i + 1)
+    if (i % 2 === 0) {
+      drawLine1(color1, 0, y1, x2, 200)
+      drawLine2(color3, 0, y1, x2, 200)
+      drawLine3(color5, 0, y1, x2, 200)
+    } else {
+      drawLine1(color2, 0, y1, x2, 200)
+      drawLine2(color4, 0, y1, x2, 200)
+      drawLine3(color6, 0, y1, x2, 200)
     }
+  }
 
-    for(var i=0; i<numLines;i++) {
-        var y1 = 200-(i*10);
-        var x2 = 200-(10*(i+1));
-        if(i%2==0)
-        {
-            drawLine1(color1,200,y1,x2,0);  
-            drawLine2(color3,200,y1,x2,0);  
-            drawLine3(color5,200,y1,x2,0);  
-              
-        }
-        else
-        {
-            drawLine1(color2,200,y1,x2,0);
-            drawLine2(color4,200,y1,x2,0);
-            drawLine3(color6,200,y1,x2,0);
-            
-        }
-        
+  for (i = 0; i < numLines; i++) {
+    y1 = 200 - (i * 10)
+    x2 = 200 - (10 * (i + 1))
+    if (i % 2 === 0) {
+      drawLine1(color1, 200, y1, x2, 0)
+      drawLine2(color3, 200, y1, x2, 0)
+      drawLine3(color5, 200, y1, x2, 0)
+    } else {
+      drawLine1(color2, 200, y1, x2, 0)
+      drawLine2(color4, 200, y1, x2, 0)
+      drawLine3(color6, 200, y1, x2, 0)
     }
+  }
 
-    for(var i=0; i<numLines;i++) {
-        var y1 = 200-(i*10);
-        var x2 = 10*(i+1);
-        if(i%2==0)
-        {
-            drawLine1(color1,0,y1,x2,0);  
-            drawLine2(color3,0,y1,x2,0);  
-            drawLine3(color5,0,y1,x2,0);  
-              
-        }
-        else
-        {
-            drawLine1(color2,0,y1,x2,0);
-            drawLine2(color4,0,y1,x2,0);
-            drawLine3(color6,0,y1,x2,0);
-            
-        }
-        
+  for (i = 0; i < numLines; i++) {
+    y1 = 200 - (i * 10)
+    x2 = 10 * (i + 1)
+    if (i % 2 === 0) {
+      drawLine1(color1, 0, y1, x2, 0)
+      drawLine2(color3, 0, y1, x2, 0)
+      drawLine3(color5, 0, y1, x2, 0)
+    } else {
+      drawLine1(color2, 0, y1, x2, 0)
+      drawLine2(color4, 0, y1, x2, 0)
+      drawLine3(color6, 0, y1, x2, 0)
     }
+  }
 
-    for(var i=0; i<numLines;i++) {
-        var y1 = i*10;
-        var x2 = 200-(10*(i+1));
-        if(i%2==0)
-        {
-            drawLine1(color1,200,y1,x2,200); 
-            drawLine2(color3,200,y1,x2,200); 
-            drawLine3(color5,200,y1,x2,200); 
-               
-        }
-        else
-        {
-            drawLine1(color2,200,y1,x2,200);
-            drawLine2(color4,200,y1,x2,200);
-            drawLine3(color6,200,y1,x2,200);
-            
-        }
-        
+  for (i = 0; i < numLines; i++) {
+    y1 = i * 10
+    x2 = 200 - (10 * (i + 1))
+    if (i % 2 === 0) {
+      drawLine1(color1, 200, y1, x2, 200)
+      drawLine2(color3, 200, y1, x2, 200)
+      drawLine3(color5, 200, y1, x2, 200)
+    } else {
+      drawLine1(color2, 200, y1, x2, 200)
+      drawLine2(color4, 200, y1, x2, 200)
+      drawLine3(color6, 200, y1, x2, 200)
     }
+  }
 }
 
-function drawLine1(color, x1, y1, x2, y2) {
-    "use strict";
-    dibujo.beginPath();
-    dibujo.strokeStyle = color;
-    dibujo.moveTo(x1,y1);
-    dibujo.lineTo(x2,y2);
-    dibujo.stroke();
-    dibujo.closePath();
+function drawLine1 (color, x1, y1, x2, y2) {
+  'use strict'
+  dibujo.beginPath()
+  dibujo.strokeStyle = color
+  dibujo.moveTo(x1, y1)
+  dibujo.lineTo(x2, y2)
+  dibujo.stroke()
+  dibujo.closePath()
 }
 
-function drawLine2(color, x1, y1, x2, y2) {
-    "use strict";
-    dibujo2.beginPath();
-    dibujo2.strokeStyle = color;
-    dibujo2.moveTo(x1,y1);
-    dibujo2.lineTo(x2,y2);
-    dibujo2.stroke();
-    dibujo2.closePath();
+function drawLine2 (color, x1, y1, x2, y2) {
+  'use strict'
+  dibujo2.beginPath()
+  dibujo2.strokeStyle = color
+  dibujo2.moveTo(x1, y1)
+  dibujo2.lineTo(x2, y2)
+  dibujo2.stroke()
+  dibujo2.closePath()
 }
 
-function drawLine3(color, x1, y1, x2, y2) {
-    "use strict";
-    dibujo3.beginPath();
-    dibujo3.strokeStyle = color;
-    dibujo3.moveTo(x1,y1);
-    dibujo3.lineTo(x2,y2);
-    dibujo3.stroke();
-    dibujo3.closePath();
+function drawLine3 (color, x1, y1, x2, y2) {
+  'use strict'
+  dibujo3.beginPath()
+  dibujo3.strokeStyle = color
+  dibujo3.moveTo(x1, y1)
+  dibujo3.lineTo(x2, y2)
+  dibujo3.stroke()
+  dibujo3.closePath()
 }
-
-
-
--- js/script.js
+++ js/script.js
@@ -1,55 +1,49 @@
-
-function updatePhotos()
-{
-  "use strict";
-  let max=11;
-  let i=1;
-  setInterval(function()
-  {
-      if(i==max)
-      { 
-        i=1;
-      }
-      document.getElementById("photos-family-friends").setAttribute("src","./img/"+i+".jpg");
-      i++;
-    }, 2000);
-  }
-
-function updateInterests() {
-  "use strict";
-  let interests = ["Visual Computing", "Cyber Security", "Business Intelligence"];
-  let photos = ["visual-computing", "cyber-security","business-intelligence"];
-  let i=2;
-  let j=1;
-  let k=0;
-  setInterval(function()
-  {
-    if(i==-1)
-    {
-       i=2;
+// eslint-disable-next-line no-unused-vars
+function loadNavBar () {
+  var link = document.querySelector('link[rel="import"]')
+  var content = link.import
+  var el = content.querySelector('.include-navbar-here')
+  document.body.appendChild(el.cloneNode(true))
+}
+// eslint-disable-next-line no-unused-vars
+function updatePhotos () {
+  'use strict'
+  const max = 11
+  let i = 1
+  setInterval(function () {
+    if (i === max) {
+      i = 1
     }
-    else if(j==-1)
-    {
-       j=2;
-    }
-    else if(k==-1)
-    {
-       k=2;
-    }
-    
-    document.getElementById("academic-title-1").innerText = interests[i];
-    document.getElementById("academic-photo-1").setAttribute("src","./img/"+photos[i]+".jpg");
-    document.getElementById("academic-title-2").innerText = interests[j];
-    document.getElementById("academic-photo-2").setAttribute("src","./img/"+photos[j]+".jpg");
-    document.getElementById("academic-title-3").innerText = interests[k];
-    document.getElementById("academic-photo-3").setAttribute("src","./img/"+photos[k]+".jpg");
-    i--;
-    j--;
-    k--;
-  }, 2000);
-
-  
-
+    document.getElementById('photos-family-friends').setAttribute('src', './img/' + i + '.jpg')
+    i++
+  }, 2000)
 }
 
-  
+// eslint-disable-next-line no-unused-vars
+function updateInterests () {
+  'use strict'
+  const interests = ['Visual Computing', 'Cyber Security', 'Business Intelligence']
+  const photos = ['visual-computing', 'cyber-security', 'business-intelligence']
+  let i = 2
+  let j = 1
+  let k = 0
+  setInterval(function () {
+    if (i === -1) {
+      i = 2
+    } else if (j === -1) {
+      j = 2
+    } else if (k === -1) {
+      k = 2
+    }
+
+    document.getElementById('academic-title-1').innerText = interests[i]
+    document.getElementById('academic-photo-1').setAttribute('src', './img/' + photos[i] + '.jpg')
+    document.getElementById('academic-title-2').innerText = interests[j]
+    document.getElementById('academic-photo-2').setAttribute('src', './img/' + photos[j] + '.jpg')
+    document.getElementById('academic-title-3').innerText = interests[k]
+    document.getElementById('academic-photo-3').setAttribute('src', './img/' + photos[k] + '.jpg')
+    i--
+    j--
+    k--
+  }, 2000)
+}
